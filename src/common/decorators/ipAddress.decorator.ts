917ec7d3c6d9594757dccf2cdc59d907a6b1288f
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2020-04-12 13:05:17 +0300

    ip log 3

--- src/common/decorators/ipAddress.decorator.ts
+++ src/common/decorators/ipAddress.decorator.ts
@@ -3,14 +3,8 @@ import * as requestIp from 'request-ip';
 import * as geoip from 'geoip-lite';
 
 export const IpAddress = createParamDecorator(async (data, req) => {
-  let ip;
-  if (req.clientIp) {
-    ip = req.clientIp;
-    return req.clientIp;
-  } else {
-    ip = await requestIp.getClientIp(req);
-  }
-  console.log('ip', ip);
+  var ip = req.headers["X-Forwarded-For"] || req.connection.remoteAddress;
+  console.log('ip: ', ip);
   let geo = await geoip.lookup(ip);
   if (geo) return geo;
   return 'N/A';

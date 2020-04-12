15b363348ce07872a61ed00e714916a12dab268f
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2020-04-12 13:35:43 +0300

    log 6

--- src/common/decorators/ipAddress.decorator.ts
+++ src/common/decorators/ipAddress.decorator.ts
@@ -3,7 +3,7 @@ import * as requestIp from 'request-ip';
 import * as geoip from 'geoip-lite';
 
 export const IpAddress = createParamDecorator(async (data, req) => {
-  var ip = req.headers["X-Real-IP"] || req.connection.remoteAddress;
+  var ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
   console.log('ip: ', ip);
   console.log(Object.keys(req.headers));
   let geo = await geoip.lookup(ip);

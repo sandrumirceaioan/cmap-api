5d53d53c81e4455f1d3f9030ee9f3e4879be3712
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2020-04-12 12:45:18 +0300

    log address

--- src/common/decorators/ipAddress.decorator.ts
+++ src/common/decorators/ipAddress.decorator.ts
@@ -2,16 +2,16 @@ import { createParamDecorator } from '@nestjs/common';
 import * as requestIp from 'request-ip';
 import * as geoip from 'geoip-lite';
 
-export const IpAddress = createParamDecorator((data, req) => {
+export const IpAddress = createParamDecorator(async (data, req) => {
   let ip;
   if (req.clientIp) {
     ip = req.clientIp;
     return req.clientIp;
   } else {
-    ip = requestIp.getClientIp(req);
+    ip = await requestIp.getClientIp(req);
   }
-
-  let geo = geoip.lookup(ip);
+  console.log('ip');
+  let geo = await geoip.lookup(ip);
   if (geo) return geo;
   return 'N/A';
 });
